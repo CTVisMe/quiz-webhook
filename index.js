85fd4cccda26aade37578d88e515fa40f46a4dc3
Chris Volinsky <ctvisme@gmail.com>
2026-04-24 17:07:13 -0400

    cc2

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/topics", (req, res) => {
+app.get("/questions", (req, res) => {
   const { topic, count } = req.query;
   let pool = TOPIC_BANK;
 
